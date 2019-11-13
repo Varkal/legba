4e72b644a9e3a61a03b6280931f8a45a32531d6e
Varkal <moreau.romain83@gmail.com>
2019-11-13 18:27:25 +0100

    :construction: import endpoint wip

--- src/controllers/import.rs
+++ src/controllers/import.rs
@@ -1,9 +1,15 @@
+use reqwest;
 use rocket::Route;
-// use reqwest::
+use std::env;
 
 #[post("/import")]
 pub fn import() -> String {
-    return String::from("toto");
+    let client = reqwest::Client::new();
+    let result = client
+        .get("https://medium.com/me/list/queue?limit=2")
+        .header("cookie", env::var("LEGBA_COOKIE").unwrap_or_default())
+        .send();
+    return result.unwrap().text().unwrap();
 }
 
 pub fn routes() -> Vec<Route> {
