4578931c9d0c313acca529722364f18269fd2b3c
Varkal <moreau.romain83@gmail.com>
2019-11-17 21:38:44 +0100

    :construction: scraping

--- src/controllers/import.rs
+++ src/controllers/import.rs
@@ -1,4 +1,5 @@
 use reqwest;
+use scraper;
 use rocket::Route;
 use std::env;
 
@@ -9,7 +10,22 @@ pub fn import() -> String {
         .get("https://medium.com/me/list/queue?limit=2")
         .header("cookie", env::var("LEGBA_COOKIE").unwrap_or_default())
         .send();
-    return result.unwrap().text().unwrap();
+
+    let html_text = result.unwrap().text().unwrap();
+
+    let document = scraper::Html::parse_document(html_text.as_str());
+    let article_selector = scraper::Selector::parse(".streamItem").unwrap();
+    let title_selector = scraper::Selector::parse("h3").unwrap();
+
+    let mut articles_title = Vec::<String>::new();
+
+    for article_element in document.select(&article_selector) {
+        for title_element in article_element.select(&title_selector) {
+            articles_title.push(title_element.text().collect())
+        }
+    }
+
+    return articles_title.join("\n");
 }
 
 pub fn routes() -> Vec<Route> {
